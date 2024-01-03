3f49899021d77453182ed8b27bb5a5be2eff3e88
Charlie Starling <charliejackstarling1@gmail.com>
2024-01-03 19:04:36 +0000

    phpjs

--- api/allData.js
+++ api/allData.js
@@ -6,9 +6,31 @@
         }
         const data = await response.json();
 		
-		console.dir(data);
+        const courseTableBody = document.getElementById("courseTableBody");
+
+        data.forEach(course => {
+            const row = document.createElement("tr");
+
+            const titleCell = document.createElement("td");
+            titleCell.textContent = course.courseTitle;
+
+            const typeCell = document.createElement("td");
+            typeCell.textContent= course.courseSubject;
+
+            const summaryCell = document.createElement("td");
+            summaryCell.textContent= course.courseSummary;
+
+    
+            row.appendChild(titleCell);
+            row.appendChild(typeCell);
+            row.appendChild(summaryCell);
+
+            courseTableBody.appendChild(row);
+        });
+
 
     } catch (error) {
         console.error("Error fetching data from the API:", error);
     }
 })();
+
