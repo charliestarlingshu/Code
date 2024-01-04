ccb18394ac4ceb3484005d718b139054daf22686
Charlie Starling <charliejackstarling1@gmail.com>
2024-01-04 02:32:51 +0000

    foo

--- contact.php
+++ contact.php
@@ -5,18 +5,45 @@
     </head>
 
     <body>
-
         <div id="container">
-
             <?php include("includes/header.php"); ?>
 
-
-            <aside>
-                
-            </aside>
-
             <main>
-                <article></article>
+                <form method="">
+
+                    <div>
+                        <label for="firstname">First Name:</label>
+                        <input 
+                            type="text" 
+                            name="firstname" 
+                            id="firstname" 
+                            placeholder="Enter First Name"
+                            required
+                        >
+                    </div>
+                    <div>
+                        <label for="email">Email Address:</label>
+                        <input type="email" name="email" id="email" required>
+                        
+                    </div>
+                    <div>
+                        <label for="comments">Comments</label>
+                        <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+                    </div>
+                    <div>
+                        <label for="marketing">How did you find out about us?</label>
+                        <select name="marketing" id="marketing">
+                            <option value="web" selected>Web Search</option>
+                            <option value="friend">Friend Reccommendation</option>
+                        </select>
+                    </div>
+
+
+                    <div>
+                        <input type="submit" value="Send Details">
+                    </div>
+                </form>
+
             </main>
 
             <?php include('includes/footer.php'); ?>
