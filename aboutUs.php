015555df0d6d736cf36d7345cd424895c975a4ec
Charlie Starling <charliejackstarling1@gmail.com>
2024-01-04 05:13:56 +0000

    sweet

--- aboutUs.php
+++ aboutUs.php
@@ -8,22 +8,21 @@
         <?php  include( 'includes/header.php'); ?>
         
         <main id="main">
-            <aside class="sidePanel">
-                <nav></nav>
-            </aside>
 
             <section class="content">
                 <div id="heading" >
                     <h1>About Us</h1>
                 </div>    
-                <article id="asideText">
-                    <p>Cantor College was established in 1989 to specialize in Computing and Design.   At Cantor College, we want to give students the education they need to achieve their career aims, leaving them equipped with the knowledge, skills and experience to succeed. Cantor College gives you the opportunities that can give you the edge when you enter the world of work, through our teaching and our links to some of the world's leading researchers and employers. 
-                    </p>
-                </article>
-                <img src="images/cantorLectureTheatre.jpg" alt="Lecture Theatre" width="45%" height="auto">
-                <article id="asideText">
-                    <p>Our students have gone on to successful careers in a wide range of industries across the globe.  Whatever your ambitions, our learning and support can help to get the most out of your time with Cantor College, both as a student and in your future career.</p>
-                </article>
+                <img id="lectureImg" src="images/cantorLectureTheatre.jpg" alt="Lecture Theatre" width="45%" height="auto">
+                <div id="aboutUsInfo">
+                    <article id="asideText">
+                        <p>Cantor College was established in 1989 to specialize in Computing and Design.   At Cantor College, we want to give students the education they need to achieve their career aims, leaving them equipped with the knowledge, skills and experience to succeed. Cantor College gives you the opportunities that can give you the edge when you enter the world of work, through our teaching and our links to some of the world's leading researchers and employers. 
+                        </p>
+                    </article>
+                    <article id="asideText">
+                        <p>Our students have gone on to successful careers in a wide range of industries across the globe.  Whatever your ambitions, our learning and support can help to get the most out of your time with Cantor College, both as a student and in your future career.</p>
+                    </article>
+                </div>
             </section>
 
         </main>
